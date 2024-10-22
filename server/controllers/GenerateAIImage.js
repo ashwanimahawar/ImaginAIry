1cdb1c05d50c3296b19e097a80559c483a14988b
Ashwani Mahawar <mahawar77ashwin@gmail.com>
2024-10-22 18:15:18 +0530

    OpenAi Latest

--- server/controllers/GenerateAIImage.js
+++ server/controllers/GenerateAIImage.js
@@ -1,36 +1,30 @@
 import * as dotenv from "dotenv";
 import { createError } from "../error.js";
-import {OpenAIApi, Configuration} from "openai";
+import OpenAI from "openai";
 
 dotenv.config();
 
 //Setup OpenAI Api Key
-const configuration = new Configuration({
-    apiKey : process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+const configuration = {
+  apiKey: process.env.OPENAI_API_KEY,
+};
+const openai = new OpenAI(configuration);
 
 //Controller to generate Image
-
 export const generateImage = async (req, res, next) => {
-    try {
-        const { prompt } = req.body;
+  try {
+    const { prompt } = req.body;
 
-        const response = await openai.createImage({
-            prompt,
-            n: 1,
-            size: "1024x1024",
-            response_format: "b64_json",
-          });
-          const generatedImage = response.data.data[0].b64_json;
-        return res.status(200).json({ imgSrc: generatedImage });
-    } catch (error) {
-        console.log(error);
-        next(
-            createError(
-                error.status,
-                error.message
-            )
-        );
-    }
+    const response = await openai.images.generate({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+    const generatedImage = response.data[0].b64_json;
+    return res.status(200).json({ imgSrc: generatedImage });
+  } catch (error) {
+    console.log(error);
+    next(createError(error.status, error.message));
+  }
 };
